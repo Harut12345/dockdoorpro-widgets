089ec260b5bda7a53a4c36b00eed83a2f360689e
Ethan <74191134+ejbills@users.noreply.github.com>
2026-03-31 13:37:22 -0700

    Set library type to dynamic for DockDoorWidgetSDK

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "DockDoorWidgetSDK",
     platforms: [.macOS(.v14)],
     products: [
-        .library(name: "DockDoorWidgetSDK", targets: ["DockDoorWidgetSDK"]),
+        .library(name: "DockDoorWidgetSDK", type: .dynamic, targets: ["DockDoorWidgetSDK"]),
     ],
     targets: [
         .target(name: "DockDoorWidgetSDK"),
